54e22b3f41abb95ed2455b5b890f59780d5b5190
Priyanshu009-commits <priyanshuindev@gmail.com>
2025-06-03 12:26:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FeedbackApp",
-})
-
-server: {
-  hmr: {
-    overlay: false
+  base: '/FeedbackApp/',
+  server: {
+    hmr: {
+      overlay: false
+    }
   }
-}
-
+})
